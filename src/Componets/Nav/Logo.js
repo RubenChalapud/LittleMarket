a62b94f0b5585d382cb7c82eb4f111be43cffcfe
daniela <daniela.tirado@uao.edu.co>
2020-10-30 16:41:28 -0500

    Empecé con el logo del nav

---
+++ src/Componets/Nav/Logo.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import Logo from "./logo.png";
+
+
+function LogoLittleMarket() {
+    return(
+        <div className="o-container-logo">
+            <img src={Logo} alt= "Little Market"></img>
+        </div>
+
+    );
+}
+export default LogoLittleMarket;
---
+++ src/Componets/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React  from "react";
+import Logo from "./Logo.js";
+
+function Nav(){
+    return(
+        <div>
+            <Logo></Logo>
+        </div>
+    );
+}
+export default Nav;
