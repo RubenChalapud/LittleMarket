0285118d31c7c30e2a20ad845302251edaf8b6c6
JuanG2 <juan.gomez_rod@uao.edu.co>
2020-10-30 16:42:37 -0500

    Empecé con la pag de emprendimientos

---
+++ src/Componets/Emprendimientos/NavImgs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DivImgs({imagen}){
+    return(
+        <div className="o-container-imgs">
+            <img src="https://escuela-emprendedores.alegra.com/wp-content/uploads/2019/12/llevar-la-contabilidad-de-tu-negocio-preview-1.png"></img>
+        </div>
+    )
+}
+
+export default DivImgs;
