c73dea8ab1e9fc9c3ed78fbc1a1979f4132a7156
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-10-30 16:43:46 -0500

    Creé la pagina del perfil de emprendimiento

---
+++ src/perfil.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './perfil.css';
+
+function perfil(){
+    return(
+        <div>
+            <nav>
+                <Nav></Nav>
+            </nav>
+        </div>
+    );
+}
+
+export default perfil;
