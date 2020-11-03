36270d49e6e31adee244ef1735ffd10c7c445176
JuanG2 <juan.gomez_rod@uao.edu.co>
2020-11-03 14:57:54 -0500

    Added padding to the categories

--- src/Componets/Emprendimientos/ContenedorCat/ContenedorCat.js
+++ src/Componets/Emprendimientos/ContenedorCat/ContenedorCat.js
@@ -8,6 +8,7 @@ function ContenedorCat(){
             <Catg ButtonIcon={"fastfood"} TextButton="Comidas"></Catg>
             <Catg ButtonIcon={"local_bar"} TextButton="Licores"></Catg>
             <Catg ButtonIcon={"local_cafe"} TextButton="Cafe"></Catg>
+            <Catg ButtonIcon={"local_pizza"} TextButton="Pizza"></Catg>
         </div>
     );
 }
