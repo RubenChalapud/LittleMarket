8fee0fd4e5fc088478aec2d66fc55f35ac73fe91
JuanG2 <juan.gomez_rod@uao.edu.co>
2020-11-04 15:28:55 -0500

    Arreglada la card y emprendimientos nice

--- src/Componets/Emprendimientos/ContenedorCard/ContenedorCard.js
+++ src/Componets/Emprendimientos/ContenedorCard/ContenedorCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Card from "./Card/Card";
 import "./ContenedorCard.css";
+import imgProducto2 from "../../Perfil/Productos/c2.jpeg";
 
 function ContenedorCard(){
     return(
         <div className="o-cards-container">
-            <Card imagen={"https://i.pinimg.com/originals/58/e2/a7/58e2a75515e24758c508823581686ba1.jpg"} titlecard={"Adidas"} paragraph="Something idk" ButtonText="More"></Card>
+            <Card imagen={"https://i.pinimg.com/originals/58/e2/a7/58e2a75515e24758c508823581686ba1.jpg"} titlecard={"Nike"} paragraph="Something idk" ButtonText="More"></Card>
             <Card imagen={"https://henryshomemadeicecream.com/wp-content/uploads/2018/12/39201744308PM_gallery01.jpg"} titlecard={"Helados"} paragraph="Something idk" ButtonText="More"></Card>
-            <Card imagen={"https://bakingfarm.co.kr/shopimages/dhkorea88/0130010001792.jpg?1454249737"} titlecard={"Postres"} paragraph="Something idk" ButtonText="More"></Card>
+            <Card imagen={imgProducto2} titlecard={"Coctel"} paragraph="Riquisimos" ButtonText="Mas"></Card>
         </div>
     );
 }
