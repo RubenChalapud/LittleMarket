4f9a38464a04e1602e8a3d7fe4d5076217c1bd45
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-11-28 16:09:06 -0500

    Se agregó direccion para pagina de busqueda

--- src/Componets/Nav/Botones/Botones.js
+++ src/Componets/Nav/Botones/Botones.js
@@ -1,10 +1,16 @@
 import React  from "react";
 import "./Botones.css"
 
+function Boton(props){
+    return(
+        <a className="o-links" href={props.url}><span class="material-icons">search</span></a>
+    );
+}
+
 function Botones(){
     return(
         <div className="o-botones-container">
-            <span class="material-icons">search</span>
+            <Boton url="busqueda"></Boton>
             <span class="material-icons"> account_circle</span>
         </div>
     );
